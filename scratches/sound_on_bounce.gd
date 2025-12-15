d6705c8fc1ab6e857d6f1e30019cac060b39e478
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2025-12-15 18:25:11 +0100

    reconfigure project addon load

--- scratches/sound_on_bounce.gd
+++ scratches/sound_on_bounce.gd
@@ -4,14 +4,14 @@ extends RigidBody3D
 @onready var audio_player: AudioStreamPlayer3D = $AudioStreamPlayer3D
 
 func _ready():
-	body_entered.connect(_on_body_entered)
-	
-	if audio_player == null:
-		audio_player = AudioStreamPlayer3D.new()
-		add_child(audio_player)
+    body_entered.connect(_on_body_entered)
+    
+    if audio_player == null:
+        audio_player = AudioStreamPlayer3D.new()
+        add_child(audio_player)
 
 func _on_body_entered(body: Node3D):
-	if body.is_in_group("Paddle"):
-		if audio_player and paddle_hit_sound:
-			audio_player.stream = paddle_hit_sound
-			audio_player.play()
+    if body.is_in_group("Paddle"):
+        if audio_player and paddle_hit_sound:
+            audio_player.stream = paddle_hit_sound
+            audio_player.play()
