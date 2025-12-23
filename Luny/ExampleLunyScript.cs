bba733b6baa345ee9c9b5650d084e7eba6b71dbe
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2025-12-23 14:14:56 +0100

    organize project contents

---
+++ Luny/ExampleLunyScript.cs
@@ -0,0 +1,17 @@
+using Luny;
+
+/// <summary>
+/// Example LunyScript that demonstrates the basic structure.
+/// This script will automatically bind to any GameObject named "ExampleLunyScript" in the scene.
+/// </summary>
+public class ExampleLunyScript : LunyScript.LunyScript
+{
+    public override void Build()
+    {
+        // TODO: In future phases, users will construct sequences here:
+        // When.KeyPressed("Space").Do(() => Audio.Play("Jump"));
+        // When.Collision().With("Enemy").Do(() => Variables["Health"] = 0);
+        // For now, this is just a placeholder demonstrating the clean API
+        LunyLogger.LogInfo($"====> {nameof(ExampleLunyScript)} runs OnStartup !!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    }
+}
